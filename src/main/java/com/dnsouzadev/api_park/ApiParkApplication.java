761e289d37adfdc0b06a70ac28b89f0e97e50fc8
Daniel Souza <workdndsza@gmail.com>
2024-07-11 12:15:32 -0300

    iniciando o projeto

---
+++ src/main/java/com/dnsouzadev/api_park/ApiParkApplication.java
@@ -0,0 +1,13 @@
+package com.dnsouzadev.api_park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiParkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnsouzadev/api_park/ApiParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnsouzadev.api_park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
