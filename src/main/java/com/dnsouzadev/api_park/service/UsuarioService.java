fc7ddcfd988c4f7f442e29a0a642fb1eec133e8e
Daniel Souza <workdndsza@gmail.com>
2024-07-11 19:09:47 -0300

    criando metodos para o usuario

--- src/main/java/com/dnsouzadev/api_park/service/UsuarioService.java
+++ src/main/java/com/dnsouzadev/api_park/service/UsuarioService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UsuarioService {
@@ -13,7 +15,26 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
 
     @Transactional
-    public Usuario salvar(Usuario usuario) {
+    public Usuario save(Usuario usuario) {
         return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario findById(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(
+        () -> new RuntimeException("Usuário não encontrado"));
+    }
+
+    @Transactional
+    public Usuario updatePassword(Long id, String password) {
+        Usuario user = findById(id);
+        user.setPassword(password);
+        usuarioRepository.save(user);
+        return user;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Usuario> findAll() {
+        return usuarioRepository.findAll();
     }
 }
--- src/main/java/com/dnsouzadev/api_park/web/controller/UsuarioController.java
+++ src/main/java/com/dnsouzadev/api_park/web/controller/UsuarioController.java
@@ -5,10 +5,9 @@ import com.dnsouzadev.api_park.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,7 +18,25 @@ public class UsuarioController {
 
     @PostMapping
     public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
-        Usuario user = usuarioService.salvar(usuario);
+        Usuario user = usuarioService.save(usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> findById(@PathVariable Long id) {
+        Usuario user = usuarioService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Usuario> updatePassword(@PathVariable Long id, @RequestBody Usuario usuario) {
+        Usuario user = usuarioService.updatePassword(id, usuario.getPassword());
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> findAll() {
+        List<Usuario> usuarios = usuarioService.findAll();
+        return ResponseEntity.ok(usuarios);
     }
 }
