50dbba5f8e41957c8e641d1f5e1b06c58dbae300
Daniel Souza <workdndsza@gmail.com>
2024-07-15 14:45:08 -0300

    configurando o ambiente de testes

---
+++ src/test/java/com/dnsouzadev/api_park/UsuarioIT.java
@@ -0,0 +1,16 @@
+package com.dnsouzadev.api_park;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/usuarios/usuario-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/usuarios/usuario-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class UsuarioIT {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+}
