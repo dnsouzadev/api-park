c741024b14e7bf9c6febbffb2fdcb23d75d4750b
Daniel Souza <workdndsza@gmail.com>
2024-07-15 18:41:12 -0300

    habilitando o sistema de auditoria

--- src/main/java/com/dnsouzadev/api_park/entity/Usuario.java
+++ src/main/java/com/dnsouzadev/api_park/entity/Usuario.java
@@ -2,6 +2,11 @@ package com.dnsouzadev.api_park.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -13,6 +18,7 @@ import java.util.Objects;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Usuario implements Serializable {
 
     @Id
@@ -27,12 +33,16 @@ public class Usuario implements Serializable {
     @Column(name = "role", nullable = false, length = 25)
     private Role role = Role.ROLE_CLIENTE;
 
+    @CreatedDate
     @Column(name = "data_criacao")
     private LocalDateTime dataCriacao;
+    @LastModifiedDate
     @Column(name = "data_modificacao")
     private LocalDateTime dataModificacao;
+    @CreatedBy
     @Column(name = "criado_por")
     private String criadoPor;
+    @LastModifiedBy
     @Column(name = "modificado_por")
     private String modificadoPor;
 
