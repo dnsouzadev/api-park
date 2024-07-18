b439ba89f13756cc1014fffee3dac1e5c50afa7b
Daniel Souza <workdndsza@gmail.com>
2024-07-18 10:43:21 -0300

    criando e mapeando a entidade Vaga e o repositorio

---
+++ src/main/java/com/dnsouzadev/api_park/entity/Vaga.java
@@ -0,0 +1,62 @@
+package com.dnsouzadev.api_park.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "vagas")
+@EntityListeners(AuditingEntityListener.class)
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Vaga implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "codigo", nullable = false, unique = true, length = 4)
+    private String codigo;
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StatusVaga status;
+
+    @CreatedDate
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+    @LastModifiedDate
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+    @CreatedBy
+    @Column(name = "criado_por")
+    private String criadoPor;
+    @LastModifiedBy
+    @Column(name = "modificado_por")
+    private String modificadoPor;
+
+    public enum StatusVaga {
+        LIVRE, OCUPADA
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vaga vaga = (Vaga) o;
+        return Objects.equals(id, vaga.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/dnsouzadev/api_park/repository/VagaRepository.java
@@ -0,0 +1,7 @@
+package com.dnsouzadev.api_park.repository;
+
+import com.dnsouzadev.api_park.entity.Vaga;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VagaRepository extends JpaRepository<Vaga, Long> {
+}
