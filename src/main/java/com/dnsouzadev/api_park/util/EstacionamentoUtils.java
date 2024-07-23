01b20bf45fdba8ec796f0924f7aa7d1179a3f5eb
Daniel Souza <workdndsza@gmail.com>
2024-07-22 23:14:21 -0300

    criando o metodo para criar recibo

---
+++ src/main/java/com/dnsouzadev/api_park/util/EstacionamentoUtils.java
@@ -0,0 +1,19 @@
+package com.dnsouzadev.api_park.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class EstacionamentoUtils {
+
+    public static String gerarRecibo() {
+        LocalDateTime date = LocalDateTime.now();
+        String recibo = date.toString().substring(0, 19);
+        return recibo.replace("-", "")
+                .replace(":", "")
+                .replace("T", "-");
+    }
+
+}
